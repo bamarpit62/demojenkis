ec17cbb42732f3caa7e4449492bcd32835d511ab
bamarpit62 <bamniyaarpit@gmail.com>
2023-08-31 15:10:08 +0530

    first repo

---
+++ Amdocsb5Application.java
@@ -0,0 +1,13 @@
+package com.demo.amdocsb5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Amdocsb5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Amdocsb5Application.class, args);
+	}
+
+}
